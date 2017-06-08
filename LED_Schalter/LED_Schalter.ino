11073917cecc204f85f27562558b019c441c2156
zjoew <zhou.wang@tu-braunschweig.de>
2017-06-08 23:11:38 +0200

    Verbesser

--- LED_Schalter/LED_Schalter.ino
+++ LED_Schalter/LED_Schalter.ino
@@ -1,18 +1,30 @@
-const byte led=13;
-const byte sw=2;
+const byte led = 13;
+const byte sw = 2;
+byte click = 0;
+boolean lastState = 0;
+boolean toggle = 0;
 void setup() {
   pinMode(led,OUTPUT);
   pinMode(sw,INPUT);
+  lastState = digitalRead(sw);
 }
 
 void loop() {
-  boolean val=digitalRead(sw);
-  if (val)
+  boolean b1 = digitalRead(sw);
+  if (b1 != lastState)
     {
-      digitalWrite(led,HIGH);
+      delay (20);
+      boolean b2 = digitalRead (sw);
+      if (b1 == b2)
+        {
+          lastState = b1;
+          click++;
+        }
     }
-   else
+   if (click == 2)
     {
-      digitalWrite(led,LOW);
+      click = 0;
+      toggle = !toggle;
+      digitalWrite (led,toggle);
     }
 }
