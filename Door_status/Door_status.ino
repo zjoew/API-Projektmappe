1e9695119608d948d963fc6dd402de6caadd393c
zjoew <zhou.wang@tu-braunschweig.de>
2017-07-06 18:21:59 +0200

    Door_status_verbessert_1

--- Door_status/Door_status.ino
+++ Door_status/Door_status.ino
@@ -1,31 +1,34 @@
 #include<LiquidCrystal.h>
 LiquidCrystal lcd(11,12,6,5,4,3);
 
+unsigned long ultras()
+{
+  digitalWrite(10,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(10,LOW);
+  return pulseIn(9,HIGH);
+}
+
 void setup() {
-  pinMode(7,INPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,INPUT);
   lcd.begin(16,2);
-  lcd.clear();
   lcd.print("Guten Tag, Boys!");
   lcd.setCursor(3,1);
   lcd.print("<Arduino>");
   delay(3000);
+  lcd.clear();
 }
 
 void loop() {
-  int foto = analogRead(A0);
-  boolean val = digitalRead(7);
-  if (foto>=1010 && val==false)
+  unsigned long d = ultras()*10/58;
+  if (d>=5)
   {
-    lcd.clear();
-    lcd.cursor();
-    lcd.blink();
-    lcd.print("Door is closed.");
+    lcd.print("Door is open!");
   }
   else
   {
-    lcd.clear();
-    lcd.cursor();
-    lcd.blink();
-    lcd.print("Door is open.");
+    lcd.setCursor(8,0);
+    lcd.print("closed!");
   }
 }
